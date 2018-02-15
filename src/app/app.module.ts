cff296cdd52fb3d11c75e06c2e0a8dd0bd659496
Tomasz Włodarczyk <twlodarczyk@europa-tu.local>
2018-02-15 14:57:02 +0100

    Finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { HowtoComponent } from './howto/howto.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { routes } from './app.routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HowtoComponent,
+    ExamplesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
